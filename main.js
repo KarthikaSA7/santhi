f668d9ebf3c0c74d434f32dff56b9a6be31691f7
karthika s <106165807+KarthikaSA7@users.noreply.github.com>
2024-08-28 15:32:54 +0530

    Update main.js

--- main.js
+++ main.js
@@ -4,28 +4,28 @@ function contactForm() {
 
 function resumeDownload() {
     var link = document.createElement('a');
-    link.href = 'static/images/Resume.pdf';
+    link.href = 'images/Resume.pdf';
     link.download = "Santhi_Resume.pdf";
     link.click();
 };
 
 function projectOneDownload() {
     var link = document.createElement('a');
-    link.href = 'static/images/project-1.pdf';
+    link.href = 'images/project-1.pdf';
     link.download = "Revit-Plan.pdf";
     link.click();
 };
 
 function projectTwoDownload() {
     var link = document.createElement('a');
-    link.href = 'static/images/project-2.pdf';
+    link.href = 'images/project-2.pdf';
     link.download = "North-Facing-Plan.pdf";
     link.click();
 };
 
 function projectThreeDownload() {
     var link = document.createElement('a');
-    link.href = 'static/images/project-3.pdf';
+    link.href = 'images/project-3.pdf';
     link.download = "North-Facing-Plan.pdf";
     link.click();
 };

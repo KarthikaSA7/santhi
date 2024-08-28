da79ecc9a70e49014992cc3cc5c7bb77b35a8aff
KarthikaSA7 <skarthika7475@gmail.com>
2024-08-28 15:20:55 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,31 @@
+function contactForm() {
+    document.getElementById("form").style.display = "block";
+};
+
+function resumeDownload() {
+    var link = document.createElement('a');
+    link.href = 'static/images/Resume.pdf';
+    link.download = "Santhi_Resume.pdf";
+    link.click();
+};
+
+function projectOneDownload() {
+    var link = document.createElement('a');
+    link.href = 'static/images/project-1.pdf';
+    link.download = "Revit-Plan.pdf";
+    link.click();
+};
+
+function projectTwoDownload() {
+    var link = document.createElement('a');
+    link.href = 'static/images/project-2.pdf';
+    link.download = "North-Facing-Plan.pdf";
+    link.click();
+};
+
+function projectThreeDownload() {
+    var link = document.createElement('a');
+    link.href = 'static/images/project-3.pdf';
+    link.download = "North-Facing-Plan.pdf";
+    link.click();
+};
